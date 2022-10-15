17c3113c01ee4ba374cace7e2c627cf63012d250
Ethan Fidler <ethancf1999@gmail.com>
2022-10-15 14:32:05 -0400

    setting up for project 1 documentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { Project1Component } from './project1/project1.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'documentation/project1', component: Project1Component},
+  // {path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { Project1Component } from './project1/project1.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    Project1Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
